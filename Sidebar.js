572a60e208f9f8c3bf3d23ede5ae0edd66c8201c
Dragomir Kapitanski <151621211+Drag0mirK@users.noreply.github.com>
2023-12-23 03:03:04 +0200

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,10 @@
+function showSidebar()
+{
+    const sidebar = document.querySelector(".side-navigatio-bar")
+    sidebar.style.display ='flex'
+}
+function closeSidebar()
+{
+    const sidebar = document.querySelector(".side-navigatio-bar")
+    sidebar.style.display ='none'
+}
